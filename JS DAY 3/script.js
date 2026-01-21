859b9a76ae14f4730d401a3b00ecde4d9c6d7c67
Harshkrmu <harshescapematrix2008@gmail.com>
2026-01-21 15:39:12 +0530

    JS DAY 3

--- JS DAY 3/script.js
+++ JS DAY 3/script.js
@@ -12,3 +12,33 @@
 // let count = str.split("r").length -1;
 // console.log(count);
 
+// ----Regular function-----
+// function demo(a,b){
+//     return a+b
+// }
+// console.log(demo(9,10))
+
+// -----Arrow function------
+// Advantage : Doesn't need return or function keyword and performs asynchronously.
+// const sample=(c,d)=>c+d ;
+// console.log(sample(2,50))
+// if we have multiple lines of code then we have to use curly brackets and return.
+// // const test=(c,d)=>{
+//     return c+d
+//     -----
+//     -----    
+// }
+
+// // setTimeout(Callback,timedelay);
+// setTimeout(function(){
+//     alert('Please sign up');
+// },3000);
+
+// setInterval(function(){
+//     console.log('Hello');
+// },2000);
+
+
+// (function(){
+//     console.log('Hello');
+// })()
