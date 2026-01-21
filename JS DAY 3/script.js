ac0499aadd05745fff04d0b6da9339c39aff4354
Harshkrmu <harshescapematrix2008@gmail.com>
2026-01-21 15:06:05 +0530

    JS DAY 3

---
+++ JS DAY 3/script.js
@@ -0,0 +1,14 @@
+// let sent = "I am a comma seperated sentence";
+// let words = sent.split("");
+// console.log(words);
+
+// let sent1 = "I am a comma seperated sentence";
+// let words1 = sent1.split("a");
+// console.log(words1);
+
+// Q1. Find out the occurence of 'r' in given sentence by using split method.
+
+// let str = "tu meri mai tera me teri tu mera"
+// let count = str.split("r").length -1;
+// console.log(count);
+
