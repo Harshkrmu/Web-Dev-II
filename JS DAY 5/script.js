882b62bbf424ed33ea2356048abce38db00468d1
Harshkrmu <harshescapematrix2008@gmail.com>
2026-01-27 12:34:15 +0530

    JS DAY 5 and JS DAY 1

---
+++ JS DAY 5/script.js
@@ -0,0 +1,37 @@
+    //                                             Execution Context
+    //                    --------------------------------------------------------------
+    //                    |                                                            |
+    //         Global Execution Context (GEC)                         Functional Execution Context (FEC)
+    //        -------------------------------                         ----------------------------------
+    //        |                             |                         |                                |
+    //  Creation Phase                Execution Phase         Creation Phase                   Execution Phase
+
+
+// var a = 10;
+// console.log(a);
+// function demo(){
+//     let b = 20;
+//     console.log(b);
+// }
+// demo()
+
+// let user = {
+//     fullname: "HARSH",
+//     address: "Delhi",
+//     mobile_no: 1234567890,
+//     hobbies: ['Reading','Writing','Singing']
+// }
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
+
+// const car = {
+//     make: 'Mahindra' ,
+//     model: 'Thar'
+// };
+// // Object.freeze(car)
+// Object.seal(car)
+// car.make='jjj'
+// car.color = 'Blue';
+// console.log(car);
+
