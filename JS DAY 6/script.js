2d69086fed05c7df3c5eead6e9d8b8b06ed3a688
Harshkrmu <harshescapematrix2008@gmail.com>
2026-01-28 15:47:47 +0530

    JS DAY 6 updated

--- JS DAY 6/script.js
+++ JS DAY 6/script.js
@@ -5,8 +5,8 @@
 // heading_1.style.color = 'Blue'
 // heading_1.style.backgroundColor = 'Yellow'
 
-const para = document.querySelector("#content p")
+// const para = document.querySelector("#content p")
 
-para.textContent = "This is textContent changed"
-para.style.color = "Blue"
-para.style.backgroundColor = "Yellow"
+// para.textContent = "This is textContent changed"
+// para.style.color = "Blue"
+// para.style.backgroundColor = "Yellow"
