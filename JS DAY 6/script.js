06af5dd0a39e76490b3d01f912da06a9c5a56c41
Harshkrmu <harshescapematrix2008@gmail.com>
2026-01-28 15:43:59 +0530

    JS DAY 6

---
+++ JS DAY 6/script.js
@@ -0,0 +1,12 @@
+// const heading_1 = document.getElementById("myHeading")
+
+
+// heading_1.textContent = "New World"
+// heading_1.style.color = 'Blue'
+// heading_1.style.backgroundColor = 'Yellow'
+
+const para = document.querySelector("#content p")
+
+para.textContent = "This is textContent changed"
+para.style.color = "Blue"
+para.style.backgroundColor = "Yellow"
