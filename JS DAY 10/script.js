22cc277215b66a202568427d9bb1c6de58388310
Harshkrmu <harshescapematrix2008@gmail.com>
2026-02-25 15:45:13 +0530

    JS DAY 10 added

--- JS DAY 10/script.js
+++ JS DAY 10/script.js
@@ -1,1 +1,11 @@
-console.log('Hello')
+let pro = new Promise((resolve,reject)=>{
+    let proposal = "False"
+    if (proposal === "True"){
+        resolve()
+    }
+    else{
+        reject()
+    }
+})
+pro.then(()=>console.log("Proposal Accepted"))
+pro.catch(()=>console.log("Proposal Rejected"))
